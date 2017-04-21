0f70c0b67df299d0652b845eebc4654e4da49794
Brian Gillingham <bgilling@pitt.edu>
2017-04-21 10:52:29 -0400

    initial commit - uses upitt_workflow and islandora_solr modules

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# islandora_inventory_audit
+# upitt_islandora_inventory_audit
+
+This is a stand-alone module that will report on the inventory of the same objects across legacy systems and compare to the current ingested objects in our Islandora systems.
---
+++ theme/audit-markup.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+        * @file
+        * editor-markup display template.
+        *
+        * Variables available:
+??? an interactive table?
+        *
+        * @see islandora_mets_editor_preprocess_islandora_upitt_view
+        */
+?>
+
+<p>Pending development.</p>

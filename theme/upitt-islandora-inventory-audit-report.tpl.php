5b7a9f8b6d65084ef4a77356958413184c51dabf
Brian Gillingham <bgilling@pitt.edu>
2017-05-09 15:51:06 +0000

    ISLANDORA-87 adding filters for reports

--- theme/upitt-islandora-inventory-audit-report.tpl.php
+++ theme/upitt-islandora-inventory-audit-report.tpl.php
@@ -1,9 +1,14 @@
 <?php print implode(" | " , $links); ?>
-<h3><?php print $table; ?></h3>
+<h3><?php print $title; ?></h3>
+  <?php print $get_from_uri; ?><hr>
 
-<?php if($count_rows): ?>
+  <?php if($count_rows): ?>
   <?php print number_format($count_rows); ?> record<?php print ($count_rows == 1) ? '' : 's'; ?><br>
 <?php endif; ?>
+
+<form action="<?php print $get_from_uri; ?>" method="get" id="inventory_audit_filtering" accept-charset="UTF-8">
+<?php print $filter_controls; ?>
+</form>
 
 <?php print $pager; ?>
 <div style="overflow-x:auto">

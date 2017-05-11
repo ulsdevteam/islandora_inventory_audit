a3857c658278f5807782c2f56ca28dac98c92e75
Brian Gillingham <bgilling@pitt.edu>
2017-05-11 20:20:04 +0000

    added total object count to the datastreams filesizes by depositor report and template

--- theme/upitt-islandora-inventory-audit-report.tpl.php
+++ theme/upitt-islandora-inventory-audit-report.tpl.php
@@ -2,8 +2,11 @@
 <h3><?php print $title; ?></h3>
   <?php print $get_from_uri; ?><hr>
 
-  <?php if($count_rows): ?>
+<?php if($count_rows): ?>
   <?php print number_format($count_rows); ?> record<?php print ($count_rows == 1) ? '' : 's'; ?><br>
+<?php endif; ?>
+<?php if($total_object_count): ?>
+  <?php print number_format($total_object_count); ?> total objects<br>
 <?php endif; ?>
 
 <form action="<?php print $get_from_uri; ?>" method="get" id="inventory_audit_filtering" accept-charset="UTF-8">

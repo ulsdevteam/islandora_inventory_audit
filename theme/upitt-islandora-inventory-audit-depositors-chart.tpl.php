3abf5bc025eb2ed0c19cf3716852a0be1baeb765
Brian Gillingham <bgilling@pitt.edu>
2017-11-12 19:57:09 -0500

    added Google Chart to depositors report

---
+++ theme/upitt-islandora-inventory-audit-depositors-chart.tpl.php
@@ -0,0 +1,27 @@
+<div id="piechart" style="width: 990px; height: 700px;"></div>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Depositor', 'Total Size'],
+<?php $last_depositor = is_array($depositor_rows) && isset($depositor_rows[count($depositor_rows)]) ? $depositor_rows[count($depositor_rows)] : ''; ?>
+
+<?php foreach ($depositors_rows as $k => $row): ?>
+          [<?php print "'" . $row['depositor'] . "', " . $row['total']; ?>]<?php print ($row['depositor'] <> $last_depositor) ? ',' : ''; ?>
+<?php endforeach; ?>
+        ]);
+
+        var options = {
+          title: 'Depositor Size - (ignoring "University of Pittsburgh" content)',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+
